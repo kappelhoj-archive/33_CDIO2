24daf2445ebff11bab289cd807c63a8211a839a0
Jeppe <keke3k2@gmail.com>
2016-10-13 10:33:05 +0200

    Created constructor

--- 33_CDIO2/src/game/Field.java
+++ 33_CDIO2/src/game/Field.java
@@ -1,5 +1,12 @@
 package game;
 
 public class Field {
-
+	private int gold;
+	private boolean extraTurn;
+	
+	public Field(int gold, boolean extraTurn) 
+	{
+		this.gold = gold;
+		this.extraTurn = extraTurn;
+	}
 }
