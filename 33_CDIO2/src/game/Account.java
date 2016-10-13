eedc514fc005a82ebc3957794c7f210d39499a92
arvid <s144265@student.dtu.dk>
2016-10-13 15:12:55 +0200

    Changed changeBalance so that it returns a boolean

--- 33_CDIO2/src/game/Account.java
+++ 33_CDIO2/src/game/Account.java
@@ -4,23 +4,26 @@ public class Account {
 
 	private int balance = 1000;
 
-	public int getBalance(){
+	public int getBalance() {
 		return balance;
 	}
-/**
- * Method to calculate the new balance based on the int fieldValue.
- */
-	public void changeBalance(int fieldValue){
-		/**This part of the method simply adds or
-		 * subtracts the field value from the player' balance.
+
+	/**
+	 * Method to calculate the new balance based on the int fieldValue.
+	 */
+	public boolean changeBalance(int fieldValue) {
+		/**
+		 * This part of the method simply adds or subtracts the field value from
+		 * the player' balance.
 		 */
 
-		balance=balance+fieldValue;
-		if (balance < 0)
-			balance=0;
-		else if(balance>3000)
-			balance=3000;
-
+		balance = balance + fieldValue;
+		if (balance < 0) {
+			balance = 0;
+			return false;
+		} else if (balance > 3000)
+			balance = 3000;
+		return true;
 	}
 
 }
