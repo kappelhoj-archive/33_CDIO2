038d24e8004539cce6c177a1c7e085575f8ea41f
arvid <s144265@student.dtu.dk>
2016-10-13 10:00:52 +0200

    Added a method movePlayer to GUIController

--- 33_CDIO2/src/game/GUIController.java
+++ 33_CDIO2/src/game/GUIController.java
@@ -7,7 +7,7 @@ import desktop_resources.GUI;
 public class GUIController {
 	Field[] fields = new Field[11];
 
-	GUIController() {
+	GUIController(String player1Name,String player2Name) {
 		// Dette er så simpelt som det kan blive.
 		for (int i = 0; i < 11; i++)
 			fields[i] = new Street.Builder()
@@ -18,6 +18,13 @@ public class GUIController {
 
 		//Create the board with the 11 fields
 		GUI.create(fields);
+		GUI.addPlayer(player1Name,1000);
 		GUI.setDice(3, 4);
 	}
+	
+
+	public void movePlayer(int playerNumber,int position){
+		
+	}
+	
 }
--- 33_CDIO2/src/game/testGui.java
+++ 33_CDIO2/src/game/testGui.java
@@ -4,7 +4,7 @@ public class testGui {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		GUIController guicon=new GUIController();
+		GUIController guicon=new GUIController("","");
 		
 	}
 
