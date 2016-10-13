65b1b929e8ffa777ab7c73f911c20ed1bb356073
arvid <s144265@student.dtu.dk>
2016-10-13 20:16:50 +0200

    Added getTurn method so that output is displayed on the screen after a turn.

--- 33_CDIO2/src/game/GUIController.java
+++ 33_CDIO2/src/game/GUIController.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.awt.Color;
+
+import desktop_codebehind.Car;
 import desktop_fields.Field;
 import desktop_fields.Street;
 import desktop_resources.GUI;
@@ -21,12 +24,29 @@ public class GUIController {
 			.build();
 
 		//Create the board with the 11 fields
-		GUI.create(fields);
-		GUI.addPlayer(players[0],1000);
-		GUI.addPlayer(players[1],1000);
+		GUI.create(fields);	
+		Car[] cars=createCars();
+		GUI.addPlayer(players[0],1000,cars[0]);
+		GUI.addPlayer(players[1],1000,cars[1]);
 		GUI.setDice(3, 4);
 	}
-	 
+	
+	 private Car[] createCars(){
+		 Car[] carArray =new Car[2];
+			carArray[0]=new Car.Builder()
+					.primaryColor(Color.BLUE)
+					.secondaryColor(Color.WHITE)
+					.typeUfo()
+					.patternFill()
+					.build();
+			carArray[1]=new Car.Builder()
+					.primaryColor(Color.RED)
+					.secondaryColor(Color.WHITE)
+					.typeUfo()
+					.patternFill()
+					.build();
+			return carArray;
+	 }
 	/**
 	 * Move a player to a Field
 	 * @param playerName Player name.
@@ -37,12 +57,18 @@ public class GUIController {
 		GUI.setCar(position, playerName);
 	}
 	
-	private void changeBalanceTo(String playerName,int balance){
-		GUI.setBalance(playerName, balance);
-	} 
 	public void getTurn(String playerName,int playerBalance,int playerPosition,int[] currentDice){	
+
+		GUI.setDice(currentDice[0],1+varians(),5+varians(), currentDice[1],3+varians(),7+varians());
+		GUI.getUserButtonPressed(GameText.rollText(playerName, currentDice)+"\n"+GameText.fieldDescription[playerPosition],"Ok");
+		GUI.setBalance(playerName, playerBalance);
+		movePlayer(playerName,playerPosition+1);
 		
 	}
+	public int varians(){
+		return (int)(Math.round(Math.random()*2-1));
+	}
+	
     public void endGame(String playerName){	
 		
 	}
