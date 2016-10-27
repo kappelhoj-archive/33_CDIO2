2e425b64769cda13a2d08cab39905d6d0395c2f9
Jeppe <keke3k2@gmail.com>
2016-10-27 10:38:31 +0200

    Updated Board with comments.

--- 33_CDIO2/src/game/Board.java
+++ 33_CDIO2/src/game/Board.java
@@ -3,12 +3,15 @@ package game;
 public class Board {
 	private Field[] fields;
 	
+	/**
+	 * Board constructor creates a board with 11 fields.
+	 */
 	public Board()
 	{
-		// Create array with gold value for each field.
+		// Create array with gold values for each field.
 		int[] gold = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
 		
-		// Create array with 11 elements.
+		// Create fields array with 11 elements.
 		fields = new Field[11];
 		
 		// Go through fields array.
@@ -26,11 +29,19 @@ public class Board {
 		}
 	}
 	
+	/**
+	 * Method getFieldGold returns the gold value of a specific field.
+	 * @return Gold value of a field.
+	 */
 	public int getFieldGold(int fieldIndex)
 	{
 		return fields[fieldIndex].getGold();
 	}
 	
+	/**
+	 * Method getFieldExtraTurn returns the extraTurn value of a specific field.
+	 * @return Extra turn value of a field.
+	 */
 	public boolean getFieldExtraTurn(int fieldIndex)
 	{
 		return fields[fieldIndex].getExtraTurn();
