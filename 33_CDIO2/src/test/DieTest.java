692d39d08e873073e879e96541e1dd67bb5e4f6a
Mikkel <Mikkel@mikkels-mbp-2.eduroam.wireless.dtu.dk>
2016-10-27 10:32:08 +0200

    Comment changes

---
+++ 33_CDIO2/src/test/DieTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DieTest 
+{
+
+	@Before
+	public void setUp() throws Exception 
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception 
+	{
+	}
+
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
