af690dd41a38eb80238530e1313f438066a761c6
Jeppe <keke3k2@gmail.com>
2016-10-27 12:04:40 +0200

    Updated die

--- 33_CDIO2/src/game/Die.java
+++ 33_CDIO2/src/game/Die.java
@@ -1,6 +1,7 @@
 package game;
 
-public class Die {
+public class Die 
+{
 
 	public final int MAX_VALUE;
 	private int value;
@@ -8,25 +9,38 @@ public class Die {
 	/**
 	 * Constructor Die creates a 6 sided die, with a random face value between 1-6.
 	 */
-	public Die() {
+	public Die() 
+	{
 		MAX_VALUE = 6;
 		value = rollDie();
 	}
 	/**
-	 * Constructor Die creates a n-sided die, with a random face value between 1-6.
+	 * Constructor Die creates a n-sided die, with a random face value between 1-n.
+	 * If the parameter gives is less than 1, the constructor constructs a 6-sided die.
+	 * If the parameter overflows, the constructor constructs a 6-sided die.
 	 * @param The amount of sides you want the die to have.
 	 */
-	public Die(int n) {
-		MAX_VALUE = n;
-		value = rollDie();
+	public Die(int n) 
+	{
+		if ( n < 1)
+		{
+			MAX_VALUE = 6;
+			value = rollDie();
+		}
+		else
+		{
+			MAX_VALUE = n;
+			value = rollDie();
+		}
 	}
 
 	/**
-	 * Method rollDie sets the face value of the die to a random integer between 1-6.
+	 * Method rollDie sets the face value of the die to a random integer between 1-MAX_VALUE.
 	 * @return Retunerer terningens værdi (int).
 	 */
-	public int rollDie() {
-		// generates a random value between 1-6.
+	public int rollDie() 
+	{
+		// generates a random value between 1-MAX_VALUE.
 		value = (int) (Math.random() * MAX_VALUE + 1);
 		return value;
 	}
@@ -35,14 +49,16 @@ public class Die {
 	 * Method getValue returns the face value of the die.
 	 * @return The current face value of the die.
 	 */
-	public int getValue() {
+	public int getValue() 
+	{
 		return value;
 	}
 
 	/**
 	 * Method toString returns a string representation of the die.
 	 */
-	public String toString() {
+	public String toString() 
+	{
 		return "The value of the die is: " + value;
 	}
 	
