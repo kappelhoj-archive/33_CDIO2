c419f7200b4b5f53674ce94ed125d84da36e997d
Jeppe <keke3k2@gmail.com>
2016-10-27 11:09:30 +0200

    Updated Field with comments.

--- 33_CDIO2/src/game/Field.java
+++ 33_CDIO2/src/game/Field.java
@@ -4,17 +4,29 @@ public class Field {
 	private int gold;
 	private boolean extraTurn;
 	
+	/**
+	 * Field constructor creates a field object with a gold value and extraTurn value.
+	 * @param Gold value and extraTurn value.
+	 */
 	public Field(int gold, boolean extraTurn) 
 	{
 		this.gold = gold;
 		this.extraTurn = extraTurn;
 	}
 	
+	/**
+	 * Method getGold returns the gold value of the field.
+	 * @return Gold value of the field.
+	 */
 	public int getGold()
 	{
 		return gold;
 	}
 	
+	/**
+	 * Method getExtraTurn returns the extraTurn value of the field.
+	 * @return Extraturn value of the field.
+	 */
 	public boolean getExtraTurn()
 	{
 		return extraTurn;
